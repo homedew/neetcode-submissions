1c1a6d7932ff370ac50c4ca7da8d037e20c1ac4b
homedew <homedew.tc@gmail.com>
2026-08-29 21:18:21 +0700

    Add: level-order-traversal-of-binary-tree - submission-9

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-9.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public List<List<int>> LevelOrder(TreeNode root) {
+        if( root == null || root.val == null) return [];
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        List<List<int>> res = new List<List<int>>();
+        while(queue.Count>0)
+        {
+            int size = queue.Count;
+            List<int> temp = new List<int>();
+            for(int i = 0 ; i < size ; i++)
+            {
+                 TreeNode node = queue.Dequeue(); 
+                 temp.Add(node.val);
+                 if(node.left !=null) queue.Enqueue(node.left);
+                 if(node.right !=null) queue.Enqueue(node.right);
+
+            }
+            
+
+            res.Add(temp);
+        }
+
+        return res;
+    }
+}
