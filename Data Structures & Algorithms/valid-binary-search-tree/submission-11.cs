7b96f8deaff14442b1ed3edbd48e1c9083d5e107
homedew <homedew.tc@gmail.com>
2026-08-31 22:55:12 +0700

    Add: valid-binary-search-tree - submission-11

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-11.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool IsValidBST(TreeNode root) {
+        return IsValid(root, long.MinValue, long.MaxValue);
+    }
+
+    private bool IsValid(TreeNode node, long left, long right)
+    {
+        if(node == null) return true;
+
+        if(!(left < node.val && node.val < right)) return false;
+        return IsValid(node.left, left, node.val) && IsValid(node.right, node.val, right);
+    }
+}
