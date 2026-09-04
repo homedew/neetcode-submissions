e74b8122a35e92278b071a1733767c467ebd3b37
homedew <homedew.tc@gmail.com>
2026-09-04 07:09:03 +0700

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.cs
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    int pre_idx = 0;
+    Dictionary<int, int> indices = new Dictionary<int, int>();
+    public TreeNode BuildTree(int[] preorder, int[] inorder) {
+        for(int i = 0 ; i < inorder.Length; i ++)
+        {
+            indices[inorder[i]] = i;
+        }
+
+        return Dfs(preorder, 0, inorder.Length - 1);
+        
+    }
+
+
+    private TreeNode Dfs(int[] preorder, int l, int r)
+    {
+        if(l > r) return null;
+
+        int rootVal = preorder[pre_idx++];
+        TreeNode root  = new TreeNode(rootVal);
+
+        int mid = indices[rootVal];
+        root.left = Dfs(preorder, l, mid - 1);
+        root.right = Dfs(preorder, mid + 1, r);
+
+        return root;
+
+    }
+}
