59e9cb78020a540cbebb46629a4a774d9e8288f5
homedew <homedew.tc@gmail.com>
2026-09-23 00:00:42 +0700

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.cs
@@ -0,0 +1,56 @@
+public class TrieNode 
+{
+    public TrieNode[] children = new TrieNode[26];
+    public bool endWord = false;
+}
+public class PrefixTree {
+    private TrieNode root;
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+    
+    public void Insert(string word) {
+        TrieNode cur = root;
+        foreach( char c in word)
+        {
+            int i = c -'a';
+            if(cur.children[i] == null)
+            {
+                cur.children[i] = new TrieNode();
+            }
+
+            cur = cur.children[i];
+        }
+
+        cur.endWord = true;
+    }
+    
+    public bool Search(string word) {
+        TrieNode cur = root;
+        foreach(char c in word)
+        {
+            int i = c -'a';
+            if(cur.children[i] == null)
+            {
+                return false;
+            }
+
+            cur = cur.children[i];
+        }
+
+        return cur.endWord;
+    }
+    
+    public bool StartsWith(string prefix) {
+        TrieNode cur = root;
+        foreach(char c in prefix)
+        {
+            int i = c - 'a';
+            if(cur.children[i] == null) return false;
+
+            cur = cur.children[i];
+        }
+
+        return true;
+    }
+}
